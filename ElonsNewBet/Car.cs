898ad42968f796a55284066ad196b9368687edb5
mart6110 <martinus.mortensen@gmail.com>
2022-08-29 16:31:25 +0200

    Add project files.

---
+++ ElonsNewBet/Car.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ElonsNewBet
+{
+    public class Car
+    {
+        // Variables
+        private byte _battery = 100;
+        private int _driveTimes;
+        private int _meters = 20;
+        private int _metersDriven;
+        private string _color;
+
+        public string Color { get => _color; set => _color = value; }
+        public int DriveTimes { get => _driveTimes; set => _driveTimes = value; }
+        public byte Battery { get => _battery; set => _battery = value; }
+        private int MetersDriven { get => _metersDriven; set => _metersDriven = value; }
+        public int Meters { get => _meters; set => _meters = value; }
+
+        public void Drive()
+        {
+            // I make a for loop to find out how much battery life their is left.
+            for(int i = 0; i < _driveTimes; i++)
+            {
+                Battery--;
+            }
+
+            // Sets the meter driven.
+            MetersDriven += Meters * DriveTimes;
+
+            Console.WriteLine("Den " + Color + " bils batteri er på: " + Battery + "%.");
+            Console.WriteLine("Den " + Color + " bil har kørt: " + MetersDriven + " meter.");
+            Console.WriteLine();
+        }
+
+        public void ChargeCar()
+        {
+            Console.WriteLine("Ladder batteriet op for den " + Color + " bil...");
+
+            // Resets the battery and meter driven.
+            Battery = 100;
+            MetersDriven = 0;
+
+            Thread.Sleep(3000);
+
+            Console.WriteLine("Batteriet er fuldt opladt.");
+            Console.WriteLine();
+            Console.WriteLine("Den " + Color + " bils batteri er på: " + Battery + "%.");
+            Console.WriteLine("Den " + Color + " bil har kørt: " + MetersDriven + " meter.");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ElonsNewBet/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ElonsNewBet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool on = true;
+
+            Car car1 = new Car();
+
+            Car car2 = new Car();
+
+            // Add color to the cars
+            car1.Color = "blå";
+            car2.Color = "rød";
+
+            // I create a do/while loop so I can keep driving the car and recharge the car.
+            // While the On bool is true.
+            do
+            {
+                // If the Battery value is 0, I call the ChargeCar method, else I call the Drive method.
+                if (car1.Battery == 0)
+                {
+                    car1.ChargeCar(); //ChargeCar method
+                }
+                else
+                {
+                    Console.WriteLine("Hvor mange gange skal " + car1.Color + " bil køre");
+                    car1.DriveTimes = Convert.ToInt32(Console.ReadLine());
+
+                    car1.Drive(); //Drive method
+                }
+
+                // If the Battery value is 0, I call the ChargeCar() method, else I call the Drive() method.
+                if (car2.Battery == 0)
+                {
+                    car2.ChargeCar(); //ChargeCar method
+                }
+                else
+                {
+                    Console.WriteLine("Hvor mange gange skal " + car2.Color + " bil køre");
+                    car2.DriveTimes = Convert.ToInt32(Console.ReadLine());
+
+                    car2.Drive(); //Drive method
+                }
+
+                Console.WriteLine();
+
+            } while (on == true);
+        }
+    }
+}
